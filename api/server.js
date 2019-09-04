66a5786b8295760c63f705439c730406faa96d45
AndrewA0112 <andrew.mkal@outlook.com>
2019-09-04 12:44:54 -0500

    added productsRouter

--- api/server.js
+++ api/server.js
@@ -1,9 +1,24 @@
 const express = require('express')
 
+const productsRouter = require('../products/products-router.js')
+
 const server = express()
+
+// global middleware
+server.use(express.json())
+
+server.use('/products', productsRouter)
+// server.use('/suppliers', suppliersRouter)
+
+
+// route handlers
 
 server.get('/', (req, res) => {
     res.status(200).json({ message: 'api running!'})
 });
+
+server.get('/clients', (req, res) => {
+    
+})
 
 module.exports = server;
---
+++ products/products-router.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.use(express.json())
+
+router.get('/', (req, res) => {
+    res.send('GET to /products/')
+})
+
+router.get('/:id', (req, res) => {
+    const { id } = req.params;
+    res.send(`GET to /products/${id}`)
+})
+
+module.exports = router;
